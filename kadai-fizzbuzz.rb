4ade60d7a06db2589dd4cb67369beefc0d93c7c7
shio.tktk <shiii000203@gmail.com>
2022-10-01 13:03:29 +0000

    third

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,16 +1,18 @@
-num_max = 100
-# numが1~100まで繰り返し実行
-(1..num_max).each do |num|
-  def fizzbuzz(num)
-    if num % 3 == 0 && num % 5 == 0
-      return 'FizzBuzz'
-    elsif num % 3 == 0
-      return 'Fizz'
-    elsif num % 5 == 0 
-      return 'Buzz'
-    else 
-      return num
-    end
+
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0
+    return 'FizzBuzz'
+  elsif num % 3 == 0
+    return 'Fizz'
+  elsif num % 5 == 0 
+    return 'Buzz'
+  else 
+    return num
   end
+end
+
+# numが1~100まで繰り返し実行
+num_max = 100
+(1..num_max).each do |num|
   puts fizzbuzz(num)
 end
